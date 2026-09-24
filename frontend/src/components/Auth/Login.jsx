259b104956e39a4f58b8d43ace1f0ccd9c98de3e
Vector_BJIT_Academy <vectorbjitacademy@gmail.com>
2026-09-24 15:44:45 +0600

    second test run for webhook testing

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -52,7 +52,7 @@ const Login = () => {
         <div className="container">
           <div className="header">
             <img src="/careerconnect-black.png" alt="logo" />
-            <h3>Login to your account Please!</h3>
+            <h3>Login to your account With your credentials please!</h3>
           </div>
           <form>
             <div className="inputTag">
